9ab6a2311d1ae068eacd355d9b147464b8a271e6
Ulvi <ulvi.amirov.2000@gmail.com>
2026-03-28 00:20:56 +0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Space_Grotesk, Unbounded } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
+import { Analytics } from '@vercel/analytics/react'
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -36,6 +37,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   )
